e32eba4bbecb1ed70e8ba5b3a89417257b07ec6a
frankkulak <fs.kulak@gmail.com>
2024-03-10 15:51:58 -0400

    Update dependencies and error handling on Discord.

--- src/pages/viewer/discord/DiscordViewerDelegate.svelte
+++ src/pages/viewer/discord/DiscordViewerDelegate.svelte
@@ -20,8 +20,10 @@
   $: fileIsHTML = extension === "html";
 
   let fetchedFileIsReady = false;
-  let fetchedFileError: string;
+  let fetchedFileErrorCode: string;
+  let fetchedFileErrorMessage: string;
   let plainTextContent: string;
+  $: hasError = Boolean(fetchedFileErrorCode || fetchedFileErrorMessage);
 
   onMount(() => {
     const apiEndpoint = window.location.hash?.replace("#/", "");
@@ -33,40 +35,37 @@
   });
 
   async function fetchPackage(endpoint: string) {
-    try {
-      const res = await fetch(`${config.API_BASE}/${endpoint}`);
-      if (res.ok) {
-        const arrayBuffer = await res.arrayBuffer();
-        const buffer = window.NodeJS.Buffer.from(arrayBuffer);
-        if (await ViewerState.loadPackage(buffer, params.filename)) {
-          fetchedFileIsReady = true;
-        } else {
-          fetchedFileError = "Package is invalid or empty.";
-          console.error(fetchedFileError);
-        }
+    safeFetch(endpoint, async (res) => {
+      const arrayBuffer = await res.arrayBuffer();
+      const buffer = window.NodeJS.Buffer.from(arrayBuffer);
+      if (await ViewerState.loadPackage(buffer, params.filename)) {
+        fetchedFileIsReady = true;
       } else {
-        console.error(res);
-        fetchedFileError = `[${res.status}] ${res.statusText}`;
+        fetchedFileErrorMessage = "Package is invalid or empty.";
       }
-    } catch (e) {
-      console.error(e);
-      fetchedFileError = e instanceof Error ? e.message : e;
-    }
+    });
   }
 
   async function fetchPlainText(endpoint: string) {
+    safeFetch(endpoint, async (res) => {
+      plainTextContent = await res.text();
+      fetchedFileIsReady = true;
+    });
+  }
+
+  async function safeFetch(endpoint: string, onOk: (res: Response) => void) {
     try {
       const res = await fetch(`${config.API_BASE}/${endpoint}`);
       if (res.ok) {
-        plainTextContent = await res.text();
-        fetchedFileIsReady = true;
+        onOk(res);
       } else {
         console.error(res);
-        fetchedFileError = `[${res.status}] ${res.statusText}`;
+        fetchedFileErrorCode = res.status.toString();
+        fetchedFileErrorMessage = await res.text();
       }
     } catch (e) {
       console.error(e);
-      fetchedFileError = e instanceof Error ? e.message : e;
+      fetchedFileErrorMessage = e instanceof Error ? e.message : e;
     }
   }
 </script>
@@ -85,16 +84,16 @@
   {/if}
 {:else}
   <section class="pt-10 h-screen w-screen flex items-center justify-center">
-    {#if Boolean(fetchedFileError)}
+    {#if Boolean(hasError)}
       <div class="text-center">
-        <h2 class="mb-4 text-2xl font-bold">Error</h2>
-        <p class="mb-8">{fetchedFileError}</p>
+        <h2 class="mb-4 text-2xl font-bold">Error {fetchedFileErrorCode}</h2>
+        <p class="mb-8">{fetchedFileErrorMessage}</p>
         <p class="text-sm text-subtle">
-          Try refreshing the page. If it still isn't working, <a
+          Not sure why you're seeing this error? <a
             href="https://frankkmods.com/#/contact"
             target="_blank"
             rel="noreferrer"
-            class="text-secondary">let me know</a
+            class="text-secondary">Contact me</a
           >.
         </p>
       </div>
